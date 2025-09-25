3760a1f14fbe936df22763f1a4acc856e5a71c51
Emily <emilywong2017@u.northwestern.edu>
2025-09-25 15:55:01 -0400

    Fix: Edited config file for Github hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://emilyw98.github.io',
+  base: '/emily-content-portfolio-pages/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
