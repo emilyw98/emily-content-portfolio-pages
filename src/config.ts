13b2cd5e7278e4e50b896889fdf953d18f13492b
Emily <emilywong2017@u.northwestern.edu>
2025-09-26 13:54:19 -0400

    Updating tab icon and title

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Emily Wong | Portfolio';
+export const SITE_DESCRIPTION = 'Emily Wong is a marketing & growth strategist with background in product management and proven track record in building and scaling content engines.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
